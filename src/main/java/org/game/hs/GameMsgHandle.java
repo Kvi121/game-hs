7767b68a27b4cac7dc80a1102d6af2d85285f0e0
zhangwenyi <zhangwenyi@zhujiayi360.com>
2023-03-07 16:26:16 +0800

    1

--- src/main/java/org/game/hs/GameMsgHandle.java
+++ src/main/java/org/game/hs/GameMsgHandle.java
@@ -2,6 +2,9 @@ package org.game.hs;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,9 +23,20 @@ public class GameMsgHandle extends SimpleChannelInboundHandler<Object> {
      */
     static private final Logger LOGGER = LoggerFactory.getLogger(GameMsgHandle.class);
 
+    private static final ChannelGroup channelGroup = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception{
+        super.channelActive(ctx);
+        channelGroup.add(ctx.channel());
+    }
 
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, Object msg) throws Exception {
         LOGGER.info("hello get msg:" + msg.getClass().getName());
+        //业务逻辑处理
+        if(msg instanceof GameMsgProtocol.UserEntryCmd){
+            GameMsgProtocol.UserEntryResult.Builder resultBuilder = GameMsgProtocol.
+        }
     }
 }
--- src/main/resources/readMe.txt
+++ src/main/resources/readMe.txt
@@ -1,4 +1,6 @@
 http://cdn0001.afrxvk.cn/hero_story/demo/step010/index.html?serverAddr=127.0.0.1:64430&userId=1
 
-
+//home
 protoc.exe --java_out=F:\project\game-hs\src\main\java .\GameMsgProtocol.proto
+//company
+protoc.exe --java_out=G:\iot project\Learning\src\main\java .\GameMsgProtocol.proto
